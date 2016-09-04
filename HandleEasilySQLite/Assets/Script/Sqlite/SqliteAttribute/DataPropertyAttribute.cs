b7a5c742e5f63f8eee79a1df5a9b44109f19a4a9
yunomichawan <ddlkgjbdnm@gmail.com>
2016-09-04 22:59:06 +0900

    スクリプト追加

---
+++ HandleEasilySQLite/Assets/Script/Sqlite/SqliteAttribute/DataPropertyAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class DataPropertyAttribute : Attribute
+{
+    public enum SQLITE_TYPE
+    {
+        TEXT,
+        NUMERIC,
+        BOOLEAN,
+        ARRAY,
+        DATETIME,
+    }
+
+    public int MaxLength { get; set; }
+    public SQLITE_TYPE SqliteType { get; set; }
+    public bool NullOk { get; set; }
+    public bool IsPrimaryKey { get; set; }
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="maxLength">最大文字数</param>
+    /// <param name="sqliteType">データ型</param>
+    /// <param name="nullOk">NULL許容</param>
+    /// <param name="isPrimaryKey">プライマリキー</param>
+    public DataPropertyAttribute(int maxLength, SQLITE_TYPE sqliteType, bool nullOk, bool isPrimaryKey)
+    {
+        this.MaxLength = maxLength;
+        this.SqliteType = sqliteType;
+        this.NullOk = nullOk;
+        this.IsPrimaryKey = isPrimaryKey;
+    }
+}
